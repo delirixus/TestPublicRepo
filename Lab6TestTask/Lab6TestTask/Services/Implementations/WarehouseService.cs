895c9401589cadd94bf8e8d75b111b9e1c06b4c3
delirixus <irixusdel@gmail.com>
2025-10-30 18:06:45 +0300

    Update WarehouseService.cs

--- Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
@@ -19,11 +19,26 @@ public class WarehouseService : IWarehouseService
 
     public async Task<Warehouse> GetWarehouseAsync()
     {
-        throw new NotImplementedException();
+        var warehouseId = await _dbContext.Warehouses
+       .Select(w => new
+       {
+           w.WarehouseId,
+           TotalValue = w.Products
+               .Where(p => p.Status == ProductStatus.ReadyForDistribution)
+               .Sum(p => p.Quantity * p.Price)
+       })
+       .OrderByDescending(x => x.TotalValue)
+       .Select(x => x.WarehouseId)
+       .FirstOrDefaultAsync();
+
+        return await _dbContext.Warehouses
+            .FirstOrDefaultAsync(w => w.WarehouseId == warehouseId);
     }
 
     public async Task<IEnumerable<Warehouse>> GetWarehousesAsync()
     {
-       throw new NotImplementedException();
+        return await _dbContext.Warehouses
+        .Where(w => w.Products.Any(p =>p.ReceivedDate >= new DateTime(2025, 4, 1) &&p.ReceivedDate <= new DateTime(2025, 6, 30))).AsNoTracking()
+         .ToListAsync();
     }
 }
